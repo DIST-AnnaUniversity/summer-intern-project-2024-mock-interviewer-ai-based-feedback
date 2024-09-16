d9ea5225354fa4150bf4fbbd1e690f8111ab77f4
Abharnashree <abharnanu@gmail.com>
2024-09-16 09:54:30 +0530

    video processing added

--- mookie/urls.py
+++ mookie/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('upload/', views.upload_resume, name='upload_resume'),
     path('update_question_index/', views.update_question_index, name='update_question_index'),
     path('interview_process/', views.interview_process, name='interview_process'),
-    #path('show_analysis/', views.show_analysis, name='show_analysis'),
+    path('video_results/', views.video_results, name='video_results'),
 ]
--- mookie/views.py
+++ mookie/views.py
@@ -18,11 +18,17 @@ import time
 from django.views.decorators.csrf import csrf_exempt
 import json
 import re
+from fer import FER
+import numpy as np 
 #import wave
 import subprocess
 #import parselmouth
 #from parselmouth.praat import call
 #import io
+
+emotions=[]
+eye_moves= {'left':0,'right':0}
+MOVEMENT_THRESHOLD = 10
 
 @csrf_exempt
 def update_question_index(request):
@@ -101,21 +107,62 @@ def logout_user(request):
 def face_detection():
     detector = dlib.get_frontal_face_detector()
 
+    predictor = dlib.shape_predictor("C:/Users/abharna shree m/Downloads/shape_predictor_68_face_landmarks.dat")
+    LEFT_EYE = list(range(36, 42))
+    RIGHT_EYE = list(range(42,48))
+    prev_left_eye_center = None
+    prev_right_eye_center = None
+    emotion_detector = FER(mtcnn=True)
+
     cap = cv2.VideoCapture(0)
 
     while True:
         ret, frame = cap.read()
+        frame = cv2.resize(frame, (320, 240)) 
+
+        frame_width= frame.shape[1]
     
         if not ret or frame is None or frame.size == 0:
             print("Frame not captured correctly")
             break
-
+        
+        result = emotion_detector.detect_emotions(frame)
+        if result:
+            ep=result[0]['emotions']
+            maxe=max(ep, key=ep.get)
+            print(f"Emotion detected: {maxe}")
+            emotions.append(maxe)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = detector(gray,1)
         for face in faces:
-            x, y, w, h = face.left(), face.top(), face.width(), face.height()
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            landmarks = predictor(gray, face)
+            landmarks = np.array([[p.x,p.y] for p in landmarks.parts()])
+
+            left_eye = landmarks[LEFT_EYE]
+            right_eye =landmarks[RIGHT_EYE]
+
+            left_eye_center = np.mean(left_eye, axis=0)
+            right_eye_center = np.mean(right_eye, axis=0)
+
+            if prev_left_eye_center is not None and prev_right_eye_center is not None:
+                left_eye_movement = np.abs(left_eye_center[0] - prev_left_eye_center[0])
+                right_eye_movement = np.abs(right_eye_center[0] - prev_right_eye_center[0])
+
+                if left_eye_movement > MOVEMENT_THRESHOLD:
+                    if left_eye_center[0] < prev_left_eye_center[0]:
+                        eye_moves['left'] += 1
+                    else:
+                        eye_moves['right'] += 1
+
+                if right_eye_movement > MOVEMENT_THRESHOLD:
+                    if right_eye_center[0] < prev_right_eye_center[0]:
+                        eye_moves['left'] += 1
+                    else:
+                        eye_moves['right'] += 1
+            prev_left_eye_center = left_eye_center
+            prev_right_eye_center = right_eye_center
+        #print(f"Eye moves left: {eye_moves['left']}, Eye moves right: {eye_moves['right']}")
 
         _, jpeg = cv2.imencode('.jpg', frame)
         frame = jpeg.tobytes()
@@ -128,6 +175,17 @@ def face_detection():
 def video_feed(request):
     return StreamingHttpResponse(face_detection(), content_type='multipart/x-mixed-replace; boundary=frame')
 
+def video_results(request):
+    print("\n INSIDE VIDEO RESULTS \n")
+    eye_moved = eye_moves.get('left', 0) + eye_moves.get('right', 0)
+    most_common_emotion = max(set(emotions), key=emotions.count) if emotions else "No emotion detected"
+    
+    print("VIDEO RESULTS : eye moved - ",eye_moved,"most common emotion - ",most_common_emotion)
+
+    return JsonResponse({
+        'eye_moved': eye_moved,
+        'most_common_emotion': most_common_emotion
+    })
 
 def summarize_content(content):
     api_url = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
@@ -224,16 +282,20 @@ def generate_questions(user):
         response = llm_chain.invoke({})
         questions_text = response["text"]  
 
-        print(questions_text)
+        print("GEBERATED CONTENT : ",questions_text,"\n")
 
-        questions_and_answers = re.split(r'\n(?=\d+\.)', questions_text.strip())
-        print(questions_and_answers)
-        questions = [qa.split("Expected Answer:")[0].strip() for qa in questions_and_answers if "Expected Answer:" in qa]
-        answers = [qa.split("Expected Answer:")[1].strip() for qa in questions_and_answers if "Expected Answer:" in qa]
+        questions_and_answers = re.split(r'(\d+\.\sQuestion:)', questions_text.strip())
+        print("QUESTIONS AND ANSWERS SPLIT : ",questions_and_answers,"\n")
+        combined = []
+        for i in range(1, len(questions_and_answers), 2):
+            combined.append(questions_and_answers[i] + questions_and_answers[i + 1])
 
+        # Extract questions and answers from combined text
+        questions = [qa.split("Expected Answer:")[0].strip() for qa in combined]
+        answers = [qa.split("Expected Answer:")[1].strip() for qa in combined]
         
-        print(questions)
-        print(answers)
+        print("QUESTIONS : ",questions,"\n")
+        print("ANSWERS : ",answers,"\n")
 
         return questions, answers
     
@@ -241,32 +303,6 @@ def generate_questions(user):
         raise Resume.DoesNotExist("No resume found for this user.")
     except Exception as e:
         raise Exception(f"An error occurred during question generation: {str(e)}")
-
-def check_grammar(text):
-    url = "https://api.languagetoolplus.com/v2/check"
-    data = {
-        'text': text,
-        'language': 'en-US',
-    }
-    response = requests.post(url, data=data)
-    result = response.json()
-
-    # Extract grammar issues from the response
-    matches = result.get('matches', [])
-    grammar_issues = []
-    
-    for match in matches:
-        issue = {
-            'message': match['message'],
-            'context': match['context']['text'],
-            'suggestions': [s['value'] for s in match.get('replacements', [])]
-        }
-        grammar_issues.append(issue)
-
-        print(grammar_issues)
-    
-    return grammar_issues
-
 
 
 
@@ -289,12 +325,9 @@ def interview_process(request):
     expected_answer = answers[current_question_index]
 
     percentage = get_similarity(user_answer, expected_answer)
-    grammar_issues = check_grammar(user_answer)
 
     request.session['best_match_percentage'] = percentage
-    request.session['grammar_issues'] = grammar_issues
 
     return JsonResponse({
         'percentage': percentage,
-        'grammar_issues': grammar_issues
     })
